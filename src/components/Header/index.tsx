041a825ebbb8da272857c76ea5395bd3f0bb00c4
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-02-21 22:47:51 -0300

    Updating React-Switch: including icons in  switch button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import Switch from 'react-switch';
 import {ThemeContext} from 'styled-components';
 import { shade} from 'polished';
+import { BiMoon, BiSun } from 'react-icons/bi';
 
 import {Container} from './styles';
 
@@ -15,15 +16,70 @@ const Header: React.FC<Props> = ({ toggleTheme }) => {
     <Container>
       Hello World
 
-
       <Switch 
         onChange={toggleTheme}
-        checked={title === 'dark'}
-        checkedIcon={false}
-        uncheckedIcon={false}
-        height={10}
-        width={40}
-        handleDiameter={20}
+        checked={title === 'dark'}        
+        handleDiameter={28}
+        height={30}
+        width={60}
+        checkedIcon={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              fontSize: 15,
+              paddingRight: 2
+            }}
+          >
+            On
+          </div>
+        }
+        uncheckedIcon={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              fontSize: 15,
+              color: "orange",
+              paddingRight: 2
+            }}
+          >
+            Off
+          </div>
+        }       
+       
+        uncheckedHandleIcon={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              color:"orange",
+              fontSize: 20
+            }}
+          ><BiSun />
+          </div>
+        }
+        checkedHandleIcon={
+         
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              color: "blue",
+              fontSize: 18
+            }}
+          >
+            <BiMoon />
+          </div>
+        }
         offColor={shade(0.15, colors.primary)}
         onColor={colors.secundary}
       />      
